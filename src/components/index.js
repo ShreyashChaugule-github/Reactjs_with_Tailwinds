995754135f8d91ce623d0c7f49d24e9706602c9d
ShreyashChaugule-github <cshreyash219@gmail.com>
2024-02-21 23:58:36 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from './Billing'
+import Business from './Business'
+import Stats from './Stats'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import Cta from './Cta'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Testimonial from './Testimonial'
+import Hero from './Hero'
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    Cta,
+    Stats,
+    Footer,
+    Testimonial,
+    Hero
+};
